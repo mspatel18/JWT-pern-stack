c479cc8369bf6ae650ad802dac085d4ae0f69a51
Mann Patel <mannpatel3118@gmail.com>
2024-01-10 23:57:09 +0530

    /auth/login

--- server/routes/jwtAuth.js
+++ server/routes/jwtAuth.js
@@ -42,4 +42,42 @@ router.post("/register", async (req, res) => {
   }
 });
 
+//login route
+router.post("/login", async (req, res) => {
+  try {
+    //1. destructure the req.body
+
+    const { email, password } = req.body;
+
+    //2. check if user doesn't exist (if not then we throw error)
+
+    const user = await pool.query("SELECT * FROM users WHERE user_email=$1", [
+      email,
+    ]);
+    if (user.rows.length === 0) {
+      return res.status(401).json("Password or Email is incorrect");
+    }
+
+    //3. check if incomming password is the same the database password
+
+    const validPassword = await bcrypt.compare(
+      password,
+      user.rows[0].user_password
+    );
+
+    if (!validPassword) {
+      return res.status(401).json("Password or Email is incorrect");
+    }
+    // console.log(validPassword);
+
+    //4. give them the jwt token
+
+    const token = jwtGenerator(user.rows[0].user_id);
+    res.json({ token });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("server error");
+  }
+});
+
 module.exports = router;
